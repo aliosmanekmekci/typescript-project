be3e22a82517f71e0e66b7f3affd5ace3b98c191
Ali Ekmekci <aliosmanekmekci@gmail.com>
2024-04-17 17:33:50 +0300

    feat: Add detail page for products in CartDetails
    - Implemented a detailed view for each product within the CartDetails component.
    - Mapped through the products to display individual details when the "Buy" button is clicked.
    - Enhanced user experience by providing more information about each product.

--- src/routes/CartDetails/CartDetails.tsx
+++ src/routes/CartDetails/CartDetails.tsx
@@ -1,42 +1,78 @@
+import { Box, Container, Table } from "@radix-ui/themes";
 import { useEffect, useState } from "react";
-import { Link } from "wouter";
+import { useParams } from "wouter";
 
 interface ProductData {
   id: string;
   title: string;
   description: string;
+  image: string;
 }
 
 export default function CartDetails() {
-  const [products, setProducts] = useState<ProductData[]>([]);
-
-  // Function to fetch product list
-  const fetchProducts = async () => {
-    try {
-      const response = await fetch(`https://fakestoreapi.com/products`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch products");
-      }
-      const data: ProductData[] = await response.json();
-      setProducts(data);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    }
-  };
+  const [product, setProduct] = useState<ProductData | null>(null);
+  const params = useParams<{ id: string }>();
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(
+          `https://fakestoreapi.com/products/${params.id}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch product");
+        }
+        const data: ProductData = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+
+    if (params.id) {
+      fetchProduct();
+    }
+  }, [params.id]);
 
   return (
     <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <Link href={`/product/${product.id}`}>
-            <a>{product.title}</a>
-          </Link>
+      {product && (
+        <div>
+          <Box
+            style={{
+              background: "var(--gray-a2)",
+              borderRadius: "var(--radius-3)",
+            }}
+          >
+            <Container size="1">
+              <Box py="9" m="-1">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  style={{ width: "100%", height: "auto" }}
+                />
+                <Table.Root>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.ColumnHeaderCell>
+                        {product.title}
+                      </Table.ColumnHeaderCell>
+                    </Table.Row>
+                  </Table.Header>
+
+                  <Table.Body>
+                    <Table.Row>
+                      <Table.RowHeaderCell>
+                        {product.description}
+                      </Table.RowHeaderCell>
+                    </Table.Row>
+                  </Table.Body>
+                </Table.Root>
+              </Box>
+            </Container>
+          </Box>
         </div>
-      ))}
+      )}
     </div>
   );
 }
